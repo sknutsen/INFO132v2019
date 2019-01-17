18ae873fef31473f00518912a54463ef1408ce46
Sondre Knutsen <sondreknutsen1@gmail.com>
2019-01-17 14:40:20 +0100

    commit

---
+++ Oblig1/Oblig1.py
@@ -0,0 +1,20 @@
+## Oppgave 1
+print("Sondre")
+print("Knutsen")
+
+print("Sondre\nKnutsen")
+
+f = "Sondre"
+e = "Knutsen"
+print(f)
+print(e)
+
+
+## Oppgave 2
+
+
+
+## Oppgave 3a
+
+
+##Oppgave 3b
