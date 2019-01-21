73341b415c959e4cc16d8af1d8d296750230e034
Sondre Knutsen <sondreknutsen1@gmail.com>
2019-01-21 17:11:51 +0100

    commit

--- Oblig1/Oblig1.py
+++ Oblig1/Oblig1.py
@@ -1,26 +1,44 @@
+# Temainnlevering 1
+
 # Oppgave 1
 print("Sondre")
-print("Knutsen")
+print("Knutsen\n")
 
-print("Sondre\nKnutsen")
+print("Sondre\nKnutsen\n")
 
 f = "Sondre"
-e = "Knutsen"
-print(f)
-print(e)
+e = "\nKnutsen"
+print(f, e, "\n")
 
 
 # Oppgave 2
-string = """\
-     ***      ***   *      *  **    **     ****
-    *        *   *  **     *  * *   * *    *
-   *        *     * * *    *  *  *  *  *   *
-   *       *
+ascii_text = """\
+ *****                                      
+*     *  ****  *    * *****  *****  ****** 
+*       *    * **   * *    * *    * *      
+ *****  *    * * *  * *    * *    * ***** 
+      * *    * *  * * *    * *****  *     
+*     * *    * *   ** *    * *   *  *       
+ *****   ****  *    * *****  *    * ******                                                                                         
 """
-print()
+print(ascii_text)
 
 
 # Oppgave 3a
+def currency_print(x):
+    euro = x * 25.62 / 250
+    dollar = x * 29.05 / 250
+    print(x, "kroner tilsvarer", euro, "Euro og", dollar, "Dollar.\n")
+
+
+currency_print(250)
 
 
 # Oppgave 3b
+def currency_print2(x):
+    euro = x * 25.62 / 250
+    dollar = x * 29.05 / 250
+    print("NOK", x, "tilsvarer", euro, u"\N{euro sign}", "og", dollar, u"\N{dollar sign}")
+
+
+currency_print2(250)
