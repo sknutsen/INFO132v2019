1a30b47f80ac51645a6d907b4c4b0cbfd267a77c
Sondre Knutsen <sondreknutsen1@gmail.com>
2019-01-23 17:07:51 +0100

    commit

---
+++ Oblig2/skn003_Oblig2.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+# Temainnlevering 2
+
+# Oppgave 1
+r = input("Radius: ")
+print("Arealet til en sirkel med radius", r, "er " + '%.3f' % (float(r)**2*math.pi))
+
+
+# Oppgave 2
+string = input("Sentence: ")
+guess = input("Guess: ")
+print("That's", str(len(string) == int(guess)) + "!!\n")
+
+
+# Oppgave 3
+n = input("Gi meg et tall: ")
+rand = str(random.randint(8, 9))
+print(n + rand + "/" + n + "=" + '%.1f' % (int(n + rand) / int(n)))
